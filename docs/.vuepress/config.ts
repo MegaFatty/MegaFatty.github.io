0757d061024e64d03d57c7afc3923cada8d8981e
Chris Liu <chrisliu6823@gmail.com>
2024-09-24 05:40:04 -0400

    update
    修改blog存储路径

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -24,6 +24,28 @@ export default defineUserConfig({
     // 添加您的部署域名
     // hostname: 'https://your_site_url',
 
+
+    blog: {
+      /**
+       * 通过 glob string 配置包含文件，
+       * 默认读取 源目录中的所有 `.md` 文件，但会排除 `notes` 配置中用于笔记的目录。
+       */
+      include: ['Chat/**/*.md'],
+      // 如果希望只将源目录下某个目录下的文章读取为博客文章，比如 `blog` 目录，可以配置为：
+      // include: ['blog/**/*.md'],
+
+      /**
+       * 通过 glob string 配置排除的文件，相对于 源目录
+       */
+      exclude: ['.vuepress/', '**/README.md'],
+
+      // 禁用分页
+      // pagination: false,
+      // 每页显示的文章数量
+      pagination: 15,
+    },
+
+
     plugins: {
       /**
        * Shiki 代码高亮
@@ -40,11 +62,11 @@ export default defineUserConfig({
        */
       markdownEnhance: {
         demo: true,
-      //   include: true,
-      //   chart: true,
-      //   echarts: true,
-      //   mermaid: true,
-      //   flowchart: true,
+        //   include: true,
+        //   chart: true,
+        //   echarts: true,
+        //   mermaid: true,
+        //   flowchart: true,
       },
 
       /**
--- docs/preview/custom-component.example copy.md
+++
@@ -1,9 +0,0 @@
----
-title: Hello
-tags:
-  - other
-createTime: 2024/09/24 
-permalink: /article/y0pimsfu/
----
-
-### This is my First Blog.
